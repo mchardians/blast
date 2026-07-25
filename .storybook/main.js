c464f0bb16af4fca62d13191c30a133cd565fb4c
Mochammad Ardiansyah <ardianbro1310@gmail.com>
2026-07-25 15:13:47 +0700

    chore: update storybook addon and remove unused dependency
    Updated the version of the @storybook/addon-designs package to ^8.2.1 for improved features and compatibility. Removed the unused dependency storybook-source-code-addon to streamline the package.json and reduce potential bloat.

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,9 +4,7 @@ const config = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
-    '@storybook/addon-designs',
-    'storybook-source-code-addon',
-    '@etchteam/storybook-addon-status'
+    '@storybook/addon-designs'
   ],
   docs: {
     autodocs: 'tag',
