400d33d1dc4593abfbd95cad328d01f478e04aa3
Mochammad Ardiansyah <ardianbro1310@gmail.com>
2026-07-26 20:25:59 +0700

    style: optimize CSS for better readability and performance
    Consolidate CSS rules for common properties and improve the layout structure in the main.css file.
    Add overflow handling to the wysiwyg component to enhance text display and prevent overflow issues.
    Refactor the docs-page layout for better responsiveness and maintainability.

--- resources/views/components/docs-page.blade.php
+++ resources/views/components/docs-page.blade.php
@@ -1,6 +1,7 @@
-<div class="blast-min-h-screen blast-bg-white">
-    <div class="blast-bg-blue-500 blast-text-white">
-        <div class="blast-container blast-flex blast-flex-col blast-pt-6 blast-pb-9 md:blast-pt-8 md:blast-pb-12">
+<div class="blast-h-screen blast-w-full blast-flex blast-flex-col blast-overflow-hidden blast-bg-white">
+
+    <div class="blast-bg-blue-500 blast-text-white blast-shrink-0">
+        <div class="blast-container blast-mx-auto blast-px-4 blast-flex blast-flex-col blast-pt-6 blast-pb-9 md:blast-pt-8 md:blast-pb-12">
             @if($label)
                 <p class="blast-text-sm blast-antialiased">
                     {{ $label }}
@@ -9,13 +10,13 @@
 
             <div class="dev-page__hero-bottom">
                 @if($title)
-                    <h1 class="blast-mt-8 md:blast-mt-10 blast-text-6xl md:blast-text-7xl blast-font-semibold blast-antialiased">
+                    <h1 class="blast-mt-8 md:blast-mt-10 blast-text-6xl md:blast-text-7xl blast-font-semibold blast-antialiased blast-break-words">
                         {{ $title }}
                     </h1>
                 @endif
 
                 @if($description)
-                    <div class="md:blast-w-10/12 blast-mt-4 md:blast-mt-5  blast-wysiwyg">
+                    <div class="md:blast-w-10/12 blast-mt-4 md:blast-mt-5 blast-wysiwyg">
                         {!! \Illuminate\Support\Str::markdown($description) !!}
                     </div>
                 @endif
@@ -23,8 +24,9 @@
         </div>
     </div>
 
-    <!-- Content -->
-    <div class=" blast-container blast-mt-6 blast-pb-12">
-        {{ $slot }}
+    <div class="blast-flex-1 blast-overflow-y-auto blast-w-full">
+        <div class="blast-container blast-mx-auto blast-px-4 blast-mt-6 blast-pb-12">
+            {{ $slot }}
+        </div>
     </div>
 </div>
